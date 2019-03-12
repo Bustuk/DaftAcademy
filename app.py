df5a6e20dc8b58400b9039efd2db7fbabcc5b7a0
Lukasz <wolodkiewiczlukasz@gmail.com>
2019-03-12 09:11:18 +0100

    My forst deploy

---
+++ app.py
@@ -0,0 +1,17 @@
+# app.py
+from flask import Flask
+from flask import request
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return 'O kurka działa!'
+
+@app.route('/request')
+def request_info():
+    return f'request method: {request.method} url: {request.url} headers: {request.headers}'
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
