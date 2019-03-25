f209aaa216c3760e186ac2bb461e111aa8197c23
Bustuk <46906474+Bustuk@users.noreply.github.com>
2019-03-25 20:58:47 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,20 @@ from flask import Flask
 from flask import request
 app = Flask(__name__)
 
+app.config['BASIC_AUTH_USERNAME'] = 'TRAIN'
+app.config['BASIC_AUTH_PASSWORD'] = 'TuN3L'
+
+basic_auth = BasicAuth(app)
+
+
+@app.route('/login', methods=['POST'])
+@basic_auth.required
+def secret_view():
+    return redirect('/hello')
+
+@app.route("/hello")
+def html():
+    return "o kurka"
 
 @app.route('/')
 def hello():
