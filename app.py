aed87f07e3baf26fec983fb4306d1af38a126743
Bustuk <46906474+Bustuk@users.noreply.github.com>
2019-03-30 12:55:08 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,54 @@
 from functools import wraps
 from uuid import uuid4, UUID
 
-from flask import Flask, request, Response, session, redirect, url_for, jsonify
+from flask import Flask, request, Response, session, redirect, url_for, jsonify,render_template
 from jinja2 import Template
 
+
+class InvalidUsage(Exception):
+    status_code = 400
+
+    def __init__(self, message, status_code=None, payload=None):
+        Exception.__init__(self)
+        self.message = message
+        if status_code is not None:
+            self.status_code = status_code
+        self.payload = payload
+
+    def to_dict(self):
+        rv = dict(self.payload or ())
+        rv['message'] = self.message
+        return rv
 
 app = Flask(__name__)
 app.secret_key = bytes.fromhex(
     'dfba670ebc21410076bb5941140e789ac6342e09c18da920'
 )
+app.fishes = {}
+
+
+def get_fish_from_json():
+    fish_data = request.get_json()
+    if not fish_data:
+        raise InvalidUsage('Please provide json data')
+    return fish_data
+
+
+def set_fish(fish_id=None, data=None, update=False):
+    if fish_id is None:
+        fish_id = str(uuid4())
+
+    if data is None:
+        data = get_fish_from_json()
+        if data is None:
+            raise InvalidUsage('Please provide json data')
+
+    if update:
+        app.fishes[fish_id].update(data)
+    else:
+        app.fishes[fish_id] = data
+
+    return fish_id
 
 
 @app.route('/')
@@ -44,7 +84,7 @@ def requires_basic_auth(func):
 @requires_basic_auth
 def login():
     session['username'] = request.authorization.username
-    return redirect('/hello')
+    return redirect('/hello/')
 
 
 def requires_user_session(func):
@@ -57,14 +97,13 @@ def requires_user_session(func):
     return wrapper
 template = Template('<p id="greeting"> Hello, {{user}}!</p>')
 
-@app.route('/hello')
+@app.route('/hello/')
 @requires_user_session
 def hello():
     x=session['username']
     render = template.render(user=str(x))
     return render
-    #return str(x) #f"<h1 id='greeting'>Hello, {{ session['username'] }}!</h1>"
-    #render_template('greeting.html', name=session['username'])
+
 
 
 @app.route('/logout/', methods=['GET', 'POST'])
@@ -76,7 +115,47 @@ def logout():
     return redirect('/')
 
 
+@app.errorhandler(InvalidUsage)
+def handle_invalid_usage(error):
+    response = jsonify(error.to_dict())
+    response.status_code = error.status_code
+    return response
 
+# @app.route('/trains', methods=['GET','POST'])
+# @requires_user_session
+# def trains():
+
+
+
+@app.route('/trains', methods=['GET', 'POST'])
+@requires_user_session
+def fishes():
+    if request.method == 'GET':
+        return jsonify(app.fishes)
+    elif request.method == 'POST':
+        fish_id = set_fish()
+        return redirect(url_for('fish', fish_id=fish_id, format='json'))
+
+
+@app.route('/trains/<fish_id>',
+           methods=['GET', 'PUT', 'PATCH', 'DELETE'])
+def fish(fish_id):
+    if fish_id not in app.fishes:
+        return 'No such fish', 404
+
+    if request.method == 'DELETE':
+        del app.fishes[fish_id]
+        return '', 204
+
+    if request.method == 'PUT':
+        set_fish(fish_id)
+    elif request.method == 'PATCH':
+        set_fish(fish_id, update=True)
+
+    if request.method == 'GET' and request.args.get('format') != 'json':
+        raise InvalidUsage("Missing 'format=json' in query string.",
+                           status_code=415)
+    return jsonify(app.fishes[fish_id])
 
 
 if __name__ == '__main__':
