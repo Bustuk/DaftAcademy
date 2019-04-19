7f0261a814406975896c5983675fa5b77b4ac1fa
Bustuk <46906474+Bustuk@users.noreply.github.com>
2019-04-19 19:57:05 +0200

    Update app.py

--- homework-login/app.py
+++ homework-login/app.py
@@ -1,14 +1,53 @@
 from functools import wraps
 from uuid import uuid4, UUID
-
 from flask import Flask, request, Response, session, redirect, url_for, jsonify
 from jinja2 import Template
 
+
+class InvalidUsage(Exception):
+    status_code = 400
+
+    def __init__(self, message, status_code=None, payload=None):
+        Exception.__init__(self)
+        self.message = message
+        if status_code is not None:
+            self.status_code = status_code
+        self.payload = payload
+
+    def to_dict(self):
+        rv = dict(self.payload or ())
+        rv['message'] = self.message
+        return rv
 
 app = Flask(__name__)
 app.secret_key = bytes.fromhex(
     'dfba670ebc21410076bb5941140e789ac6342e09c18da920'
 )
+app.trains = {}
+
+
+def get_train_from_json():
+    train_data = request.get_json()
+    if not train_data:
+        raise InvalidUsage('Please provide json data')
+    return train_data
+
+
+def set_train(train_id=None, data=None, update=False):
+    if train_id is None:
+        train_id = str(uuid4())
+
+    if data is None:
+        data = get_train_from_json()
+        if data is None:
+            raise InvalidUsage('Please provide json data')
+
+    if update:
+        app.trains[train_id].update(data)
+    else:
+        app.trains[train_id] = data
+
+    return train_id
 
 
 @app.route('/')
@@ -44,7 +83,7 @@ def requires_basic_auth(func):
 @requires_basic_auth
 def login():
     session['username'] = request.authorization.username
-    return redirect('/hello')
+    return redirect('/hello/')
 
 
 def requires_user_session(func):
@@ -57,14 +96,13 @@ def requires_user_session(func):
     return wrapper
 template = Template('<p id="greeting"> Hello, {{user}}!</p>')
 
-@app.route('/hello')
+@app.route('/hello/')
 @requires_user_session
 def hello():
     x=session['username']
     render = template.render(user=str(x))
     return render
-    #return str(x) #f"<h1 id='greeting'>Hello, {{ session['username'] }}!</h1>"
-    #render_template('greeting.html', name=session['username'])
+
 
 
 @app.route('/logout/', methods=['GET', 'POST'])
@@ -76,7 +114,45 @@ def logout():
     return redirect('/')
 
 
+@app.errorhandler(InvalidUsage)
+def handle_invalid_usage(error):
+    response = jsonify(error.to_dict())
+    response.status_code = error.status_code
+    return response
 
+
+
+
+
+@app.route('/trains', methods=['GET', 'POST'])
+@requires_user_session
+def trains():
+    if request.method == 'GET':
+        return jsonify(app.trains)
+    elif request.method == 'POST':
+        train_id = set_train()
+        return redirect(url_for('train', train_id=train_id, format='json'))
+
+
+@app.route('/trains/<train_id>',
+           methods=['GET', 'PUT', 'PATCH', 'DELETE'])
+def train(train_id):
+    if train_id not in app.trains:
+        return 'No such train', 404
+
+    if request.method == 'DELETE':
+        del app.trains[train_id]
+        return '', 204
+
+    if request.method == 'PUT':
+        set_train(train_id)
+    elif request.method == 'PATCH':
+        set_train(train_id, update=True)
+
+    if request.method == 'GET' and request.args.get('format') != 'json':
+        raise InvalidUsage("Missing 'format=json' in query string.",
+                           status_code=415)
+    return jsonify(app.trains[train_id])
 
 
 if __name__ == '__main__':
